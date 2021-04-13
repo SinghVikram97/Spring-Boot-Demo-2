a5f471cd67b5021ed1665967f73c80648946bf1f
Vikram <vbedi@expediagroup.com>
2021-04-13 10:11:58 +0530

    Overiden equals and hashcode method

--- src/main/java/com/example/springbootdemo2/model/Book.java
+++ src/main/java/com/example/springbootdemo2/model/Book.java
@@ -1,6 +1,7 @@
 package com.example.springbootdemo2.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -60,5 +61,18 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
